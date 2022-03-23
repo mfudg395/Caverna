eace70f8af6c346720479e1427f5f1da0cc5bd17
Matthew Fudge <mfudg395@mtroyal.ca>
2022-03-23 17:43:48 -0600

    implemented monk chasing player around

--- src/ground_monk/ground_monk.gd
+++ src/ground_monk/ground_monk.gd
@@ -1,10 +1,26 @@
 extends KinematicBody2D
 
 export var gravity := 835.0
-export var speed := 250.0
+export var speed := 300.0
 
 var velocity = Vector2()
+var is_facing_right := false
+onready var player = get_parent().get_node("Player")
+
+func _ready() -> void:
+	$AnimationPlayer.play("idle")
 
 func _physics_process(delta: float) -> void:
+	if is_facing_right:
+		$Sprite.set_flip_h(false)
+	else:
+		$Sprite.set_flip_h(true)
+	
+	if velocity.x != 0:
+		$AnimationPlayer.play("move")
+	
+	var direction = -1 if player.position.x < position.x else 1
+	is_facing_right = false if direction == -1 else true
+	velocity.x = speed * direction
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
