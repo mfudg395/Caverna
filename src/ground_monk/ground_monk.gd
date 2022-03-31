e19cfb8ca9a7ccf5a3a472bf22f79e816ee10e69
Nahuel Paladino <43184105+npala043@users.noreply.github.com>
2022-03-31 17:27:35 -0600

    pushing for matthew

--- src/ground_monk/ground_monk.gd
+++ src/ground_monk/ground_monk.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 export var gravity := 835.0
-export var speed := 300.0
+export var speed := 250.0
 
 var velocity = Vector2()
 var is_facing_right := false
@@ -11,16 +11,31 @@ func _ready() -> void:
 	$AnimationPlayer.play("idle")
 
 func _physics_process(delta: float) -> void:
-	if is_facing_right:
-		$Sprite.set_flip_h(false)
-	else:
-		$Sprite.set_flip_h(true)
-	
-	if velocity.x != 0:
-		$AnimationPlayer.play("move")
-	
 	var direction = -1 if player.position.x < position.x else 1
 	is_facing_right = false if direction == -1 else true
-	velocity.x = speed * direction
+	if $AnimationPlayer.current_animation != "attack1":
+		$AnimationPlayer.play("move")
+		if is_facing_right:
+			self.scale.x = 1
+		else:
+			self.scale.x = -1
+		velocity.x = speed * direction
+	else:
+		velocity.x = 0
 	velocity.y += gravity * delta
 	velocity = move_and_slide(velocity, Vector2.UP)
+	
+func hit():
+	$AttackDetector.monitoring = true
+	
+func end_of_hit():
+	$AttackDetector.monitoring = false
+	
+#func start_walk():
+#	$AnimationPlayer.play("move")
+
+func _on_PlayerDetector_body_entered(_body):
+	$AnimationPlayer.play("attack1")
+
+func _on_AttackDetector_body_entered(_body):
+	pass # Player takes damage here
