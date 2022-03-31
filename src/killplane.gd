9b896667952b87893137d6729245f4321fe31f6b
Anthony Dang <adang937@mtroyal.ca>
2022-03-30 18:52:17 -0600

    created basic map

---
+++ src/killplane.gd
@@ -0,0 +1,20 @@
+extends Area2D
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+
+func _on_Area2D_body_entered(body):
+	get_parent().get_node("Player").position = get_parent().get_node("Position2D").position
